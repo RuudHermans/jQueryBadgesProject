5683c46133e443c544219ea723f0bfcc7422cf90
RuudHermans <ruud1984.rh@gmail.com>
2017-07-03 18:11:45 +0200

    Codeschool project 1st try

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/3834045.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      for (var i = 0; i < response.courses.completed.length; i++) {
+        var title = response.courses.completed[i].title;
+        var url = response.courses.completed[i].url;
+        var badge = response.courses.completed[i].badge;
+        $("#badges").append('<div class="course"><h3>' + title + '</h3><img src="' + badge + '"><a href="' + url + '" target="_blank" class="btn btn-primary">See Course</a></div>');
+      }
+    }
+  });
 });
